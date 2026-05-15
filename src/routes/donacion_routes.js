6cf536b946b780f488ab0787fda51018c21ee40e
Xavier <luis.ochoa02@epn.edu.ec>
2026-05-15 08:21:14 -0500

    Delete src directory

--- src/routes/donacion_routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { donarPlataforma } from "../controllers/donacion_controller.js";
-
-const router = Router();
-
-// Ruta pública única para donaciones a la plataforma
-router.post("/", donarPlataforma);
-
-export default router;
