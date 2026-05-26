c0e76804fc53cee5293a2f834a99368209a8256b
Xavier <luis.ochoa02@epn.edu.ec>
2026-05-26 14:31:50 -0500

    Update Estudiante.js

--- src/models/Estudiante.js
+++ src/models/Estudiante.js
@@ -121,7 +121,7 @@ const usuarioSchema = new Schema(
           'Electromecánica',
           'Redes y Telecomunicaciones',
           'Procesamiento de Alimentos',
-          'Procesamiento industrial de la madera',
+          'Procesamiento Industrial de la Madera',
         ],
         message: 'La carrera "{VALUE}" no es válida. Las carreras permitidas son: Agua y Saneamiento Ambiental, Desarrollo de Software, Electromecánica, Redes y Telecomunicaciones, Procesamiento de Alimentos, Procesamiento Industrial de la madera',
       },
