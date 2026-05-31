734602f8340f3d048a7b72e6c03b29c07d3136a2
Xavier <luis.ochoa02@epn.edu.ec>
2026-05-31 11:11:46 -0500

    Update proyecto_controller.js

--- src/controllers/proyecto_controller.js
+++ src/controllers/proyecto_controller.js
@@ -420,7 +420,7 @@ export const crearNuevaVersion = async (req, res) => {
       colaboradores:     versionActual.colaboradores,
       estado:            'pendiente',
       motivoRechazo:     '',
-      enviarAlAdmin:     req.body.enviarAlAdmin === false || req.body.enviarAlAdmin === 'false' ? false : true,
+      enviarAlAdmin:     req.body.enviarAlAdmin === true  || req.body.enviarAlAdmin === 'true',
       publico:           versionActual.publico,
       activo:            true,
       titulo:            versionActual.titulo,
